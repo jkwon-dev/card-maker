1629e6b7fdaa5cb0f47585e5813c9f2f702d64ca
jkwon <kwonejk@gmail.com>
2022-06-11 01:43:23 +0800

    change firebase version

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,7 +1,7 @@
-//import firebase from 'firebase/app'; 
-//import 'firebase/auth';
-//import 'firebase/database';
-import firebase from 'firebase';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
