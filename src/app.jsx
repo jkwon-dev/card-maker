0b262d7ff8b6678a3b96b1cc782f4481b008b56e
jkwon <kwonejk@gmail.com>
2022-06-14 21:48:28 +0800

    add login and logout

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,21 @@
 import styles from './app.module.css';
 import Login from './components/login/login';
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import Maker from './components/maker/maker';
 
 function App({authService}) {
   return (
-    <div class={styles.app}>
-    <Login authService={authService}/>
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+          <Login authService={authService}/>
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService}/>
+          </Route>  
+        </Switch> 
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
+    const history = useHistory();
+    const goTomaker = (userId) => {
+        history.push({
+            pathname: '/maker', 
+            state: {id: userId},
+        });
+    }
     const onLogin = (event) => {
         authService.login(event.currentTarget.textContent)
-        .then(console.log); 
+        .then(data=> goTomaker(data.user.uid)); 
     }; 
+
+    useEffect(() => {
+        authService.onAuthChange(user => {
+            user && goTomaker(user.uid);
+        })
+    }); 
 
     return (
         <section className={styles.login}>
